7cef5b2c5cc5678642359e2f4beb6f4474c9509b
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2021-07-21 13:02:48 -0300

    created the create feature

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import list_products
+from .views import list_products, create_product
 
 urlpatterns = [
     path('', list_products, name='list_products'),
+    path('new', create_product, name='create_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from .forms import ProductForm
 
 
 def list_products(request):
     products = Product.objects.all()
     return render(request, 'products.html', {'products': products})
+
+
+def create_product(request):
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('list_products')
+
+    return render(request, 'products-form.html', {'form': form})
